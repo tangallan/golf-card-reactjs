844facdd08126fce5fcee65eb09c872d0ebc6d36
Allan Tang (NBC Universal) <allan.tang@nbcuni.com>
2019-04-19 12:36:59 -0700

    Started to use firebase db and re-designing game container

---
+++ src/containers/GamePlaying/GamePlaying.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class GamePlaying extends Component {
+    state = {
+
+    };
+
+    componentWillMount() {
+    }
+};
+
+export default GamePlaying;
